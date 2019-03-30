501ec85a409383002d281d63975d8ea4b3ade1e5
Marcus Balbi <balbimarcus@gmail.com>
2019-03-30 18:32:03 -0300

    stylles

--- web/src/App.js
+++ web/src/App.js
@@ -2,11 +2,12 @@ import React from 'react'
 import Header from './components/layout/Header'
 import Main from './components/layout/Main'
 import Footer from './components/layout/Footer'
+import './assets/styles/main.scss'
 const App = () => {
   return (
     <div className="root" >
       <Header />
-      <Main />
+      <Main />  
       <Footer />
     </div>
   )
