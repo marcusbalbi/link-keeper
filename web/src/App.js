a87f6e309b36550ac31ca2b6a4d7b27f3210b4c8
Marcus Balbi <balbimarcus@gmail.com>
2019-04-06 09:14:28 -0300

    config inside the project and sass

--- web/src/App.js
+++ web/src/App.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Header from './components/layout/Header'
 import Main from './components/layout/Main'
 import Footer from './components/layout/Footer'
-import './assets/styles/main.scss'
 const App = () => {
   return (
     <div className="root" >
--- web/src/index.js
+++ web/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "./sass/main.scss"
+
 import App from './App'
 
 ReactDOM.render(<App />, document.getElementById('root'));
