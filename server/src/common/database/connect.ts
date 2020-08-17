7820c4298fddd145b758e0adec224905e078232f
Marcus Balbi <balbimarcus@gmail.com>
2020-08-17 20:51:44 -0300

    add mongo service

--- server/src/common/database/connect.ts
+++ server/src/common/database/connect.ts
@@ -1,8 +1,11 @@
 import mongoose from 'mongoose';
 
-const connect = (baseName: string = process.env.MONGO_DATABASE_NAME) => {
-  const conUrl = `${process.env.MONGO_URL}/${baseName}`;
-  console.log('connection to mongo', conUrl);
+const connect = (
+  baseName: string = process.env.MONGO_DATABASE_NAME,
+  username: string = process.env.MONGO_INITDB_ROOT_USERNAME,
+  password: string = process.env.MONGO_INITDB_ROOT_PASSWORD,
+) => {
+  const conUrl = `mongodb://${username}:${password}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${baseName}?authSource=admin`;
   return mongoose.connect(conUrl, {
     useNewUrlParser: true,
   });
