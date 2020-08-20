a4809a1a00c13da8f16f019266bce11ab503794a
Marcus Balbi <balbimarcus@gmail.com>
2020-08-20 19:17:41 -0300

    test sh

---
+++ .docker/prepare_local_server.sh
@@ -0,0 +1,4 @@
+echo $PORT $LINK_ENCRYPT_KEY $MONGO_HOST $MONGO_PORT $MONGO_DATABASE_NAME
+ssh balbi@172.17.0.1 'mkdir -p projetos/deploy/link-keeper/server'
+ssh balbi@172.17.0.1 "export PORT=$PORT; LINK_ENCRYPT_KEY=$LINK_ENCRYPT_KEY; MONGO_HOST=$MONGO_HOST; MONGO_PORT=$MONGO_PORT; MONGO_DATABASE_NAME=$MONGO_DATABASE_NAME"
+ssh balbi@172.17.0.1 '/usr/bin/docker volume create link_keeper_volume'
