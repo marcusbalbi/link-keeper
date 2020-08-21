540edb49f74886e5482bb373ae8a62b329b6b6be
Marcus Balbi <balbimarcus@gmail.com>
2020-08-21 10:35:40 -0300

    title search basic

--- src/bookmarks/http/controllers/BookmarksController.ts
+++ src/bookmarks/http/controllers/BookmarksController.ts
@@ -1,11 +1,25 @@
 import { Request, Response } from 'express';
 import Bookmark from '@bookmarks/database/Bookmark';
 
+interface BookMarkFilter {
+  title?: string;
+  domain?: string;
+}
+
 export default {
   index(req: Request, res: Response) {
-    Bookmark.find({}).then((list) => {
-      res.json({ data: list });
-    });
+    const filter: BookMarkFilter = req.query;
+    const preparedFilter: any = {};
+    if (filter.title) {
+      preparedFilter.title = { $regex: new RegExp(`^${filter.title}.*$`, 'i') };
+    }
+    Bookmark.find(preparedFilter)
+      .then((list) => {
+        res.json({ data: list });
+      })
+      .catch((err) => {
+        res.status(500).json({ message: 'Erro na busca' });
+      });
   },
   show(req: Request, res: Response) {
     Bookmark.findById(req.params.id)
