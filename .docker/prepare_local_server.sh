4f7874b2a75fe90387cfb5e442378bbe1cfc8d81
Marcus Balbi <balbimarcus@gmail.com>
2020-08-21 09:43:03 -0300

    testing deploy after image updated

--- .docker/prepare_local_server.sh
+++ .docker/prepare_local_server.sh
@@ -4,5 +4,5 @@ MONGO_HOST=$MONGO_HOST
 MONGO_PORT=$MONGO_PORT 
 MONGO_DATABASE_NAME=$MONGO_DATABASE_NAME" > .env
 cat .env
-ssh balbi@172.17.0.1 'docker-compose down; docker-compose pull'
+ssh balbi@172.17.0.1 'docker-compose -f ../docker-compose.yml down;'
 ssh balbi@172.17.0.1 'mkdir -p projetos/deploy/link-keeper/server; /usr/bin/docker volume create link_keeper_volume'
