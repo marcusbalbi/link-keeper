d3460080cab98e4cad85813679c832d0794de629
Marcus Balbi <balbimarcus@gmail.com>
2020-08-21 09:23:47 -0300

    compose down

--- .docker/prepare_local_server.sh
+++ .docker/prepare_local_server.sh
@@ -4,4 +4,5 @@ MONGO_HOST=$MONGO_HOST
 MONGO_PORT=$MONGO_PORT 
 MONGO_DATABASE_NAME=$MONGO_DATABASE_NAME" > .env
 cat .env
+ssh balbi@172.17.0.1 'docker-compose down'
 ssh balbi@172.17.0.1 'mkdir -p projetos/deploy/link-keeper/server; /usr/bin/docker volume create link_keeper_volume'
